cdba9b7a42f53c40e05c4c873214725192ede956
José Antonio Leal de Farias <jalf@outlook.com>
2025-05-25 16:29:10 -0300

    Minor updates

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,37 +2,42 @@
 
 const int switch_pin = 5;
 const int relay_pin = 2;
-const int period = 5000;
+const int period = 5000;       // 5 seconds to wait
+const int minThreshold = 200;  //200mA
 
 bool powerConnected = false;
 bool shutingDown = false;
 unsigned long start_time = 0;
+int minCurrent;
 
-ACS712  ACS(A0, 5.0, 1023, 66);
+ACS712 ACS(A0, 5.0, 1023, 66);
 
-void setup() 
-{
+void setup() {
   pinMode(relay_pin, OUTPUT);
   pinMode(switch_pin, INPUT);
 
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("Started");
 
   // Initialize the current sensor
   ACS.autoMidPoint();
+
+  // Set the initial current value
+  minCurrent = ACS.mA_AC();
+  if (minCurrent < minThreshold)
+    minCurrent = minThreshold;
 }
 
-void loop() 
-{
+void loop() {
   // Read the current input
   int mA = ACS.mA_AC();
-  // Read the switch 
+  // Read the switch
   int switch_status = digitalRead(switch_pin);
-  
+
   // Check if we need turn on the vacuum
-  if (!powerConnected && (mA>200 || switch_status == HIGH))
-  {
+  if (!powerConnected && (mA > minCurrent || switch_status == HIGH)) {
     powerConnected = true;
     shutingDown = false;
     digitalWrite(relay_pin, HIGH);
@@ -40,18 +45,19 @@ void loop()
   }
 
   // Check if we need sign to turn off the vacuum
-  if (switch_status == LOW && powerConnected && mA < 100)
-  {
-      powerConnected = false;
-      shutingDown = true;
-      start_time = millis();
-      Serial.println("Turning off...");
+  if (switch_status == LOW && powerConnected && mA < minCurrent) {
+    powerConnected = false;
+    shutingDown = true;
+    start_time = millis();
+    Serial.println("Turning off...");
   }
 
   // Delay 5 seconds to turn off the vacuum
-  if(shutingDown && (millis() - start_time > period)){
-      digitalWrite(relay_pin, LOW);
-      shutingDown = false;
-      Serial.println("OFF");
+  if (shutingDown && (millis() - start_time > period)) {
+    digitalWrite(relay_pin, LOW);
+    shutingDown = false;
+    Serial.println("OFF");
   }
+
+  delay(100);  // wait to refresh
 }
